431f9f52e7afa053ecc6b1bce1664f8af90ea9c9
mastep <igorchurikov@ya.ru>
2022-10-01 01:15:11 +0300

    Сохранение фото сделано в storage

--- app/Http/Controllers/Forms/RegisterController.php
+++ app/Http/Controllers/Forms/RegisterController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers\Forms;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RegisterController
 {
     public function store(Request $request){
         $data=json_decode($request->getContent(), true);
         $name = $data['userName'];
-        User::create(["name"=>$name, 'email'=>'t@test'.time().'.ru', 'password'=>"dsadsad"])->save();
+        $photo = $data['userPhoto'];
+        $user=User::create(["name"=>$name, 'email'=>'t@test'.time().'.ru', 'password'=>"dsadsad"]);
+
+        if(!empty($photo)){
+            list($type, $b64) = explode(';base64,', $photo);
+            $b64 = base64_decode($b64);
+            $type=preg_replace("/^(.*)?\//", "",$type);
+            if(in_array($type, ['jpg','jpeg', 'png', 'gif'])){
+                $nameFile=$user->id.".".$type;
+                $Storage=Storage::disk('public');
+                $path="users/photo/".$nameFile;
+                if($Storage->put($path, $b64)){
+                    $user->photo="/storage/".$path;
+                    $user->save();
+                }
+
+            }
+        }
+
         return response(['message'=>$data['userName']], 200)
             ->header('Content-Type', 'application/json');
     }
     public function index(){
-        $users = User::orderByDesc('id')->get();
+        $users = User::orderByDesc('id')->take(30)->get();
         return response(['data'=>$users], 200)
             ->header('Content-Type', 'application/json');
     }
